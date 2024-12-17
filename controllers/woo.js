73723f3c130c730be27924817789d2df8aa336e7
Lead Web <thelettuce@rumourmedia.com>
2024-12-17 16:39:45 -0600

    eod commit

--- controllers/woo.js
+++ controllers/woo.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const axios = require("axios");
 const { response } = require("express");
 
-// WooCommerce REST API credentials
 const API_URL = process.env.WC_API_URL;
 const CONSUMER_KEY = process.env.WC_CONSUMER_KEY;
 const CONSUMER_SECRET = process.env.WC_CONSUMER_SECRET;
@@ -31,7 +30,6 @@ const getCategories = async () => {
 };
 
 const getProductsByCategory = async (id) => {
-
   try {
     const response = await axios.get(`${API_URL}/products?category=${id}`, {
       auth: {
@@ -42,15 +40,80 @@ const getProductsByCategory = async (id) => {
 
     const products = response.data;
     const productInfo = products.map((product) => ({
-        id: product.id,
-        name: product.name,
-        sku: product.sku,
+      id: product.id,
+      name: product.name,
+      sku: product.sku,
+      variations: product.variations,
     }));
 
     console.log(productInfo);
-
   } catch (error) {
     console.error("Error fetching Products by category.", error.message);
+  }
+};
+
+const getProductById = async (id) => {
+  try {
+    const response = await axios.get(`${API_URL}/products/${id}`, {
+      auth: {
+        username: CONSUMER_KEY,
+        password: CONSUMER_SECRET,
+      },
+    });
+
+    const product = response.data;
+    const productInfo = {
+      id: product.id,
+      name: product.name,
+      sku: product.sku,
+      variations: product.variations,
+    };
+    console.log(productInfo);
+
+    const productId = product.id;
+    const productName = product.name;
+    const productSku = product.sku;
+    const productVariations = product.variations;
+
+    if (productVariations && productVariations.length > 0) {
+      console.log("This product has variations");
+
+      const variationPromises = productVariations.map((variationId) => {
+        console.log(`Preparing to fetch details for variation ID: ${variationId}`);
+        return getVariationInfo(productId, variationId);
+      });
+
+      await Promise.all(variationPromises);
+      console.log("received all variation info.");
+    } else {
+      console.log("This product does not have variations");
+    }
+  } catch (error) {
+    console.error("Error fetching product.", error.message);
+  }
+};
+
+const getVariationInfo = async (prodId, varId) => {
+  try {
+    const response = await axios.get(`${API_URL}/products/${prodId}/variations/${varId}`, {
+        auth: {
+          username: CONSUMER_KEY,
+          password: CONSUMER_SECRET,
+        },
+      }
+    );
+
+    const variation = response.data;
+    const variationInfo = {
+      id: variation.id,
+      sku: variation.sku,
+      stock: variation.stock_quantity,
+    };
+
+    console.log(variationInfo);
+    return variationInfo;
+  } catch (error) {
+    console.error("Error fetching variation.", error.message);
   }
 };
 
@@ -59,10 +122,10 @@ const getAllProducts = async () => {
     let products = [];
     let page = 1;
 
+    // Fetch products in pages
     while (true) {
       console.log(`Fetching page ${page}...`);
 
-      // Make a GET request to fetch products
       const response = await axios.get(`${API_URL}/products`, {
         params: {
           consumer_key: CONSUMER_KEY,
@@ -73,7 +136,7 @@ const getAllProducts = async () => {
       });
 
       // Add the current page's products to the list
-      products = products.concat(response.data);
+      products.push(...response.data);
 
       // Check if we've fetched all pages
       if (response.data.length < 100) break;
@@ -81,14 +144,28 @@ const getAllProducts = async () => {
       page++;
     }
 
-    // Extract IDs and SKUs
+    console.log("Total products fetched:", products.length);
+
+    // TODO: Push simple products to DB
+
+    // TODO: Get variation info for variable products
+
     const productData = products.map((product) => ({
       id: product.id,
-      sku: product.sku || null, // Some products may not have SKUs
+      sku: product.sku,
+      variations: product.variations,
     }));
+    // console.log(productData);
 
-    console.log("Total products fetched:", productData.length);
-    return productData;
+    let simpleProducts = [];
+    let variableProducts = [];
+
+    simpleProducts = productData.filter( (product) => product.variations.length == 0 );
+    variableProducts = productData.filter( (product) => product.variations && product.variations.length > 0 );
+    console.log("Total variable products:", variableProducts.length);
+    console.log("Total simple products:", simpleProducts.length);
+    // console.table(variableProducts);
+
   } catch (error) {
     console.error(
       "Error fetching products:",
@@ -97,7 +174,9 @@ const getAllProducts = async () => {
   }
 };
 
-module.exports = { getCategories, getProductsByCategory, getAllProducts };
-
-// Fetch products
-// getAllProducts();
+module.exports = {
+  getCategories,
+  getProductsByCategory,
+  getProductById,
+  getAllProducts,
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,67 +1,75 @@
-const express = require('express');
-const { getAllProducts, getCategories, getProductsByCategory } = require('../controllers/woo');
+const express = require("express");
+const {
+  getAllProducts,
+  getCategories,
+  getProductsByCategory,
+  getProductById,
+} = require("../controllers/woo");
 const router = express.Router();
 
-
-
-router.get('/test-endpoint', async (req, res) => {
-    try {
-        res.json({ message: 'Connection Successful' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-    }
+router.get("/test-endpoint", async (req, res) => {
+  try {
+    res.json({ message: "Connection Successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
-router.get('/woo-get-categories', async (req, res) => {
-    try {
-        getCategories();
-        res.json({ message: 'Categories received' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-    }
-
+router.get("/woo-get-categories", async (req, res) => {
+  try {
+    getCategories();
+    res.json({ message: "Categories received" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
-
-router.get('/woo-get-prod-data', async (req, res) => {
-    try {
-        getAllProducts();
-        res.json({ message: 'Request sent'})
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-    }
+router.get("/woo-get-prod-by-id", async (req, res) => {
+  try {
+    getProductById(13934);
+    res.json({ message: "Product data received" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
-router.get('/woo-get-prod-by-cat', async (req, res) => {
-    try {
-        getProductsByCategory(158);
-        res.json({ message: 'Request sent'})
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-    }
-})
+router.get("/woo-get-prod-data", async (req, res) => {
+  try {
+    getAllProducts();
+    res.json({ message: "Request sent" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
 
-router.post('/inventory-update', async (req, res) => {
-    try {
-        const item = req.body;
-        const itemName = item.name;
-        const itemSKU = item.sku;
-        const quantity = item.quantityOnHand;
+router.get("/woo-get-prod-by-cat", async (req, res) => {
+  try {
+    getProductsByCategory(158);
+    res.json({ message: "Request sent" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
 
-        console.log(itemName + ' has ' + quantity + ' on hand.');
+router.post("/inventory-update", async (req, res) => {
+  try {
+    const item = req.body;
+    const itemName = item.name;
+    const itemSKU = item.sku;
+    const quantity = item.quantityOnHand;
 
-        res.status(200).send('Request Received');
+    console.log(itemName + " has " + quantity + " on hand.");
 
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
+    res.status(200).send("Request Received");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
